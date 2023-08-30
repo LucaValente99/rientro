06111207f1331dd1ecda5a4249b1a060059ad8a0
arnerim <arnerim@NBK04269.int.master.lan>
2023-08-30 15:02:06 +0200

    security

---
+++ rientro/src/main/java/com/luca/rientro/security/AmministratoreDetailsService.java
@@ -0,0 +1,48 @@
+package com.luca.rientro.security;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import com.luca.rientro.businesscomponent.model.Amministratore;
+import com.luca.rientro.repository.AmministratoreRepository;
+
+@Component
+public class AmministratoreDetailsService implements UserDetailsService {
+
+	private AmministratoreRepository amministratoreRepository;
+
+	private Map<String, Integer> failedAttempts = new HashMap<>();
+
+	public AmministratoreDetailsService(AmministratoreRepository amministratoreRepository) {
+		this.amministratoreRepository = amministratoreRepository;
+	}
+
+	public void incrementFailedAttempts(String username) {
+		failedAttempts.put(username, failedAttempts.getOrDefault(username, 0) + 1);
+	}
+
+	public int getFailedAttempts(String username) {
+		return failedAttempts.getOrDefault(username, 0);
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		try {
+			Amministratore amministratore = amministratoreRepository.findByUsername(username).get();
+			if (amministratore != null) {
+				return User.withUsername(amministratore.getUsername()).accountLocked(!amministratore.isEnabled())
+						.password(amministratore.getPassword()).roles(amministratore.getRole()).build();
+			}
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		}
+		throw new UsernameNotFoundException(username);
+	}
+
+}
---
+++ rientro/src/main/java/com/luca/rientro/security/SecurityConfig.java
@@ -0,0 +1,73 @@
+package com.luca.rientro.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.academy.project.repository.AmministratoreRepository;
+
+
+@Configuration
+public class SecurityConfig {
+	private AmministratoreRepository amministratoreRepository;
+
+	public SecurityConfig(AmministratoreRepository amministratoreRepository) {
+		this.amministratoreRepository = amministratoreRepository;
+	}
+	
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+		http.authorizeHttpRequests()
+		 .antMatchers("/")
+         .permitAll()
+         .antMatchers("/admin/**")
+         .hasRole("ADMIN")
+         .and()
+         .formLogin()
+         .loginPage("/")
+         .loginProcessingUrl("/login")
+         .failureHandler(customAuthenticationFailureHandler()) // Usa il gestore delle eccezioni di autenticazione personalizzato
+         .successHandler(loginSuccessHandler())
+         .permitAll()
+         .and()
+         .logout()
+         .logoutRequestMatcher(new AntPathRequestMatcher("/logoutAdmin"))
+         .logoutSuccessUrl("/admin/");
+		
+		return http.build();
+	}
+	
+	 @Bean
+	    public SimpleUrlAuthenticationSuccessHandler loginSuccessHandler() {
+	        SimpleUrlAuthenticationSuccessHandler successHandler = new SimpleUrlAuthenticationSuccessHandler();
+	        successHandler.setDefaultTargetUrl("/admin/home"); // Reindirizza gli utenti alla pagina "/admin/" dopo il login con successo
+	        return successHandler;
+	    }
+	 
+	 @Bean
+	    public CustomAuthenticationFailureHandler customAuthenticationFailureHandler() {
+	        return new CustomAuthenticationFailureHandler();
+	    }
+	
+	@Bean
+	DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+		authProvider.setUserDetailsService(new AmministratoreDetailsService(amministratoreRepository));
+		authProvider.setPasswordEncoder(passwordEncoder());
+		return authProvider;
+	}
+	
+	@Bean
+	BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+
+	
+
+}
